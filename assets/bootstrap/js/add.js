f571dfe62d76842cdd28ccf3e21370f77f9610f8
Enrickzz <enrico_cuison@dlsu.edu.ph>
2020-09-10 15:49:49 +0800

    Fix Navigation bars
    + changing active links

--- assets/bootstrap/js/add.js
+++ assets/bootstrap/js/add.js
@@ -1,11 +1,18 @@
 
 
-function opentab(tabname) {
+function opentab(tabname , thisbtn) {
     var i;
     var x = document.getElementsByClassName("card-body");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     document.getElementById(tabname).style.display = "block";
+    
+    var z = document.getElementsByClassName("nav-link");
+    for(i=0; i< z.length; i++){
+      z[i].classList.remove("active");
+    }
+    
+    thisbtn.classList.add("active");
   }
   
