c915ea0c805a80761997fbe405d9911f23b7dc1f
Enrickzz <enrico_cuison@dlsu.edu.ph>
2020-09-10 22:21:43 +0800

    Screens converted to HBS
    - active links not yet functional

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,66 @@ const { isPublic, isPrivate } = require('../middlewares/checkAuth');
 
 
 router.get('/home', isPrivate, function(req, res) {
-    // The render function takes the template filename (no extension - that's what the config is for!)
-    // and an object for what's needed in that template
-    res.render('home', {
-      layout: 'main',
-      title: 'Dashboard - Brand'
-    })
-  });
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('home', {
+    layout: 'main',
+    title: 'Dashboard - Brand'
+  })
+});
+
+router.get('/productgroup', isPrivate, function(req, res) {
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('productgroup', {
+    layout: 'main',
+    title: 'Product Groups'
+  })
+});
+router.get('/allproducts', isPrivate, function(req, res) {
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('products', {
+    layout: 'main',
+    title: 'Product List'
+  })
+});
+
+
+router.get('/manageusers', isPrivate, function(req, res) {
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('manageusers', {
+    layout: 'main',
+    title: 'Manage Users'
+  })
+});
+
+router.get('/productgroupcard', isPrivate, function(req, res) {
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('productgroup-card', {
+    layout: 'main',
+    title: '{{ Product Group }}'
+  })
+});
+
+router.get('/productcard', isPrivate, function(req, res) {
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('product-card', {
+    layout: 'main',
+    title: '{{ Product name}}'
+  })
+});
+
+router.get('/profile', isPrivate, function(req, res) {
+  // The render function takes the template filename (no extension - that's what the config is for!)
+  // and an object for what's needed in that template
+  res.render('profile', {
+    layout: 'main',
+    title: 'My Profile'
+  })
+});
 
   module.exports = router;
