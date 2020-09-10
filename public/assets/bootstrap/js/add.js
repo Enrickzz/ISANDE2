2b306ecd6ea479e780e4b83288759a5c4663dd21
Enrickzz <enrico_cuison@dlsu.edu.ph>
2020-09-10 22:00:10 +0800

    Revert "Screens converted to HBS"
    This reverts commit 41ab7d2a87ae4d86842e671f62c5a5b5058bb034.

--- public/assets/bootstrap/js/add.js
+++ public/assets/bootstrap/js/add.js
@@ -1,18 +1,19 @@
 
 
 function opentab(tabname , thisbtn,page) {
-  var i;
-  var x = document.getElementsByClassName("card-body");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    var i;
+    var x = document.getElementsByClassName("card-body");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabname).style.display = "block";
+    
+    var z = document.getElementsByClassName("nav-link");
+    for(i=0; i< z.length; i++){
+      z[i].classList.remove("active");
+    }
+    y=document.getElementById(page);
+    y.classList.add("active");
+    thisbtn.classList.add("active");
   }
-  document.getElementById(tabname).style.display = "block";
   
-  var z = document.getElementsByClassName("nav-link");
-  for(i=0; i< z.length; i++){
-    z[i].classList.remove("active");
-  }
-  y=document.getElementById(page);
-  y.classList.add("active");
-  thisbtn.classList.add("active");
-}
