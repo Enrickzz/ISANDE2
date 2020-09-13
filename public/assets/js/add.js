08b52567ccf217fbfab2324d143285acfb220ac1
Wilford Madrazo <wilford_madrazo@dlsu.edu.ph>
2020-09-13 08:22:43 +0800

    Added active state on sidebar

---
+++ public/assets/js/add.js
@@ -0,0 +1,8 @@
+$(function() {
+// remove any current navbar active classes
+$(".navbar .nav-item.active").removeClass('active');
+// add active class to proper navbar item that matches window.location
+$('.navbar .nav-item a[href="' + location.pathname + '"]').closest('li').addClass('active');
+
+$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+});
