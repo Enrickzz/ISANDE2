b99d05aac65f5920bc07862097ab6b902cc90094
Wilford Madrazo <wilford_madrazo@dlsu.edu.ph>
2020-09-22 22:02:02 +0800

    Added supplier registration and validation

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -25,3 +25,67 @@ exports.getID = (req, res) => {
     }
   });
 };
+
+exports.register = (req, res) => {
+  // 1. Validate request
+
+  // 2. If VALID, find if email exists in users
+  //      NEW USER (no results retrieved)
+  //        a. Hash password
+  //        b. Create user
+  //        c. Redirect to login page
+  //      EXISTING USER (match retrieved)
+  //        a. Redirect user to login page with error message.
+
+  // 3. If INVALID, redirect to register page with errors
+  const errors = validationResult(req);
+
+  const { company, first_name, last_name, email, mobileno} = req.body;
+    
+    if (errors.isEmpty()) {      
+    supplierModel.getOne({ company: company }, (err, result) => {
+      if (result) {
+        console.log("Company already taken!");
+        // found a match, return to login with error
+      req.flash('error_msg', 'Company already taken.');
+      //   req.session.save( function(){ res.redirect('/'); })
+      res.redirect('/supplier');
+
+      } else {
+
+          const newSupplier = {
+            company,
+            first_name,
+            last_name,
+            mobileno,
+            email
+          };
+        
+          supplierModel.register(newSupplier, (err, supplier) => {
+            if (err) {
+              req.flash('error_msg', 'Could not create supplier. Please try again.');
+              console.log(err.errors);
+              result = { success: false, message: "Supplier was not created!" }
+              //res.send(result); this causes an error LOL :<
+              res.redirect('/supplier');
+   
+            } else {
+              console.log("Successfully added supplier!");
+              console.log(supplier);
+              result = { success: true, message: "Supplier created!" }
+              //res.send(result); this causes an error LOL :<
+
+              req.flash('success_msg', 'Supplier added.');
+              res.redirect('/supplier');
+            }
+          });
+      }
+    });
+  } 
+  else {
+    const messages = errors.array().map((item) => item.msg);
+
+    req.flash('error_msg', messages.join(' '));
+    res.redirect('/supplier'); // making this redirect to / makes req.flash not appear
+  }
+};
--- models/supplier.js
+++ models/supplier.js
@@ -2,9 +2,10 @@ const mongoose = require('./connection');
 
 const supplierSchema = new mongoose.Schema(
   {
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
     company: {type: String, required: true},
-    contactperson: {type: String, required: true},
-    mobile: {type: String, required: true},
+    mobileno: {type: String, required: true},
     email: {type: String, required: true}
   }
 
@@ -24,3 +25,19 @@ exports.getAll = (param, next) => {
       next(err, post);
     });
   };
+
+// Retrieving just ONE supplier based on a query (first one)
+exports.getOne = function(query, next) {
+  supplierModel.findOne(query, function(err, supplier) {
+    next(err, supplier);
+  });
+};
+
+// Saving a supplier given the validated object
+exports.register = function(obj, next) {
+  const supplier = new supplierModel(obj);
+  
+  supplier.save(function(err, supplier) {
+    next(err, supplier);
+  });
+}
